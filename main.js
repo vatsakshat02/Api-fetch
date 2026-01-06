fca5e9ff70b07614c092c7579b283d59e4fd2674
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-07 02:24:26 +0530

    chore: added data

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const butt = document.getElementById('gettext');
 const gets = document.getElementById('getusers');
 const posts =  document.getElementById('getposts');
+const addpost = document.getElementById('addPosts')
 butt.addEventListener('click',getText);
 async function getText(){
    const res = await fetch('sample.txt');
@@ -48,4 +49,24 @@ function getPosts(){
         })
         document.getElementById('output').innerHTML = output;
     })
+}
+
+addpost.addEventListener('submit', addPosts)
+
+function addPosts(e){
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain , */*',
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify({title:title, body:body})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 }
